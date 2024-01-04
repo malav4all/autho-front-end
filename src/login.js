4c7101124f7fc9e0452ddb0f20a5e033eb5d13d9
Malav <malav.naagar@imzcorporate.com>
2024-01-04 14:54:57 +0530

    some chnages

--- src/login.js
+++ src/login.js
@@ -2,9 +2,26 @@ import { useAuth0 } from "@auth0/auth0-react";
 import React from "react";
 
 const Login = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { loginWithRedirect, user, isAuthenticated, logout } = useAuth0();
 
-  return <button onClick={() => loginWithRedirect()}>Log In</button>;
+  return (
+    <>
+      {isAuthenticated && <>{user.email}</>}
+      {isAuthenticated ? (
+        <>
+          <button
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
+          >
+            Log Out
+          </button>
+        </>
+      ) : (
+        <button onClick={() => loginWithRedirect()}>Log In</button>
+      )}
+    </>
+  );
 };
 
 export default Login;
